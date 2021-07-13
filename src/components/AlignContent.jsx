187ecc94e43092115888065a291cab4fa04ef7e4
Roman Dubovik <ip.romandubovik@gmail.com>
2021-07-13 16:02:59 +0300

    fix: spacing in Reasons list for mobile view

--- src/components/AlignContent.jsx
+++ src/components/AlignContent.jsx
@@ -10,7 +10,7 @@ const AlignContent = ({ align = 'center', leftContent, rightContent }) => {
     );
 
   if (align === 'left') {
-    const rightClass = !leftContent ? 'col offset-sm-4' : 'col';
+    const rightClass = !leftContent ? 'col offset-sm-4' : 'col mt-3 mt-sm-0';
 
     return (
       <div className='row align-items-center'>
@@ -23,7 +23,9 @@ const AlignContent = ({ align = 'center', leftContent, rightContent }) => {
   if (align === 'right') {
     return (
       <div className='row align-items-center'>
-        <div className='col-sm-8 order-2 order-sm-1'>{leftContent}</div>
+        <div className='col-sm-8 order-2 order-sm-1 mt-3 mt-sm-0'>
+          {leftContent}
+        </div>
         {rightContent && (
           <div className='col-sm-4 order-1 order-sm-2'>{rightContent}</div>
         )}
