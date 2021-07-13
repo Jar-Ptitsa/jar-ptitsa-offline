1f6a51ccd5fc2bdb5016f04f16781b481e020371
Roman Dubovik <ip.romandubovik@gmail.com>
2021-07-13 15:08:14 +0300

    fix: key value changed to unique. fix: image preview in cms when new pic is uploaded

--- src/components/Treasure.jsx
+++ src/components/Treasure.jsx
@@ -5,22 +5,30 @@ import Image from './Image';
 import VideoDetail from './VideoDetail';
 
 // render teachers photos
-const renderList = (list) => {
+const renderList = (list, getAsset) => {
   return list.map((image) => {
+    const keyId = image.image.childImageSharp
+      ? image.image.childImageSharp.fluid.src
+      : image.image; // TODO when getAsset will be involved
+
     return (
-      <div className='col-6 col-sm-4' key={image.image}>
-        <Image image={image.image} className='img-fluid w-75 mx-auto d-block' />
+      <div className='col-6 col-sm-4' key={keyId}>
+        <Image
+          image={image.image}
+          getAsset={getAsset}
+          className='img-fluid w-75 mx-auto d-block'
+        />
       </div>
     );
   });
 };
 
-const Treasure = ({ title, teachers, dance }) => {
+const Treasure = ({ title, teachers, dance, getAsset = null }) => {
   return (
     <section>
       <h2 className='fw-normal'>{title}</h2>
       <ReactMarkdown className='' children={teachers.description} />
-      <div className='row mb-3'>{renderList(teachers.list)}</div>
+      <div className='row mb-3'>{renderList(teachers.list, getAsset)}</div>
       <div className='row'>
         <div className='col-md-4 pt-3 order-2 order-md-1'>
           <VideoDetail videoId={dance.video} />
