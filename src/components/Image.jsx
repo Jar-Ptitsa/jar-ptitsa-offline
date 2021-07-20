97bcc226e090c21a0a121d912c42709c3fc861cc
Roman Dubovik <ip.romandubovik@gmail.com>
2021-07-20 16:32:38 +0300

    upd: style prop support added in Image component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import Img from 'gatsby-image';
 
-const Image = ({ image, getAsset, className = '' }) => {
+const Image = ({ image, getAsset, className = '', style = '' }) => {
   if (!image) return null;
 
   if (getAsset) {
     const imageObj = getAsset(image);
-    return <img src={imageObj.url} className={className} alt='' />;
+    return (
+      <img src={imageObj.url} className={className} alt='' style={style} />
+    );
   }
 
   if (!!image.childImageSharp) {
     return (
-      <Img fluid={image.childImageSharp.fluid} className={className} alt='' />
+      <Img
+        fluid={image.childImageSharp.fluid}
+        className={className}
+        alt=''
+        style={style}
+      />
     );
   }
 
   if (!!image && typeof image === 'string') {
-    return <img src={image} className={className} alt='' />;
+    return <img src={image} className={className} alt='' style={style} />;
   }
 };
 
