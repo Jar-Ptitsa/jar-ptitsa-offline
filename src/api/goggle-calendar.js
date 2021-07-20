49d7c3411a29630c8597013b55549df6f432a0fe
Roman Dubovik <ip.romandubovik@gmail.com>
2021-07-20 19:03:16 +0300

    fix: removed superagent npm package (replaced with axios) to fix eror during build process

--- src/api/goggle-calendar.js
+++ src/api/goggle-calendar.js
@@ -1,14 +1,15 @@
-import request from 'superagent';
+import axios from 'axios';
 import { API_KEY } from '../config';
 
 const CALENDAR_ID = 'sc3ec17dmrb689vp57jb27sq34@group.calendar.google.com';
 let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`;
 
 export function getEvents(callback) {
-  request.get(url).end((err, resp) => {
-    if (!err) {
+  axios
+    .get(url)
+    .then((resp) => {
       const events = [];
-      JSON.parse(resp.text).items.map((event) => {
+      resp.data.items.map((event) => {
         events.push({
           start: event.start.date || event.start.dateTime,
           end: event.end.date || event.end.dateTime,
@@ -17,6 +18,8 @@ export function getEvents(callback) {
         return null;
       });
       callback(events);
-    }
-  });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
