ee70ba8b385ac3213e4608b2474db006d09e6fcb
Roman Dubovik <ip.romandubovik@gmail.com>
2021-07-20 20:11:04 +0300

    fix: calendar styles - swithed off gatsby-plugin-purgecss -v

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,13 +76,13 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: false, // Activates purging in npm run develop
-        purgeOnly: [''], // applies purging only on the these file/folders
-      },
-    }, // must be after other CSS plugins
+    // {
+    //   resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+    //   options: {
+    //     develop: false, // Activates purging in npm run develop
+    //     purgeOnly: [''], // applies purging only on the these file/folders
+    //   },
+    // }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 };
