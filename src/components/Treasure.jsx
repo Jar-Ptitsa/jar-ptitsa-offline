06f9717fb3e74f10d07e718ccf716d65a149b335
Roman Dubovik <ip.romandubovik@gmail.com>
2021-07-20 15:45:43 +0300

    fix: code cleaned in Treasure component

--- src/components/Treasure.jsx
+++ src/components/Treasure.jsx
@@ -10,7 +10,7 @@ const renderList = (list, getAsset) => {
   return list.map((image) => {
     const keyId = image.image.childImageSharp
       ? image.image.childImageSharp.fluid.src
-      : image.image.url; // TODO when getAsset will be involved - check
+      : image.image.url;
 
     return (
       <div className='col-6 col-sm-4' key={keyId}>
