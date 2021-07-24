83715596b3873dacebb82e1b7e396cef96cba86c
Roman Dubovik <ip.romandubovik@gmail.com>
2021-07-24 19:06:04 +0300

    upd: content on policy page

--- src/components/Policy.jsx
+++ src/components/Policy.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Link } from 'gatsby';
 
 const Policy = ({ title, description }) => {
   return (
@@ -7,7 +9,14 @@ const Policy = ({ title, description }) => {
         <h2 className='fw-normal text-center text-sm-start text-uppercase'>
           {title}
         </h2>
-        <div>{description}</div>
+        <div>
+          <ReactMarkdown className='' children={description} />
+        </div>
+        <div className='d-flex justify-content-center'>
+          <Link to='/' className='btn btn-md btn-success'>
+            Я СОГЛАСЕН
+          </Link>
+        </div>
       </div>
     </section>
   );
